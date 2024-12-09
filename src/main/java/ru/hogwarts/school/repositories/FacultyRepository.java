49b82c73ab4287d824ead7a86fbd09675de7e6e2
bogomolova <rospisdyaika@gmail.com>
2024-12-09 03:05:49 +0200

    Hogwarts (SQL) + liquibase

--- src/main/java/ru/hogwarts/school/repositories/FacultyRepository.java
+++ src/main/java/ru/hogwarts/school/repositories/FacultyRepository.java
@@ -1,10 +1,11 @@
 package ru.hogwarts.school.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.hogwarts.school.model.Faculty;
 
 import java.util.Collection;
-
+@Repository
 public interface FacultyRepository extends JpaRepository<Faculty,Long>{
     Faculty findByNameIgnoreCase(String name);
     Collection<Faculty> findByColorContainsIgnoreCase(String color);
--- src/main/java/ru/hogwarts/school/repositories/StudentRepository.java
+++ src/main/java/ru/hogwarts/school/repositories/StudentRepository.java
@@ -4,14 +4,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.model.StudentInterface;
 
 import java.util.Collection;
 import java.util.List;
-
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
     Student findByNameIgnoreCase(String name);
+//    List<Student> getStudentsByName(String name);
     Collection<Student> findByAge(Integer age);
     Collection<Student> findByNameContainsIgnoreCase(String part);
 
